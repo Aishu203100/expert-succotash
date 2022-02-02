232707edff6e4f178d7950652a552151894b88a5
Aishu203100 <79292874+Aishu203100@users.noreply.github.com>
2022-02-01 19:32:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,48 @@
-function setup(){
-video = createCapture(VIDEO);
-video.size(654,500);
+text_box= "";
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+noseX = 0;
+noseY = 0;
 
-canvas = createCanvas(500,500);
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    video.position(200,200)
+  
+    canvas = createCanvas(550, 500);
+    canvas.position(900,200);
+  
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+  }
+  
+  function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+  }
 
-poseNet = ml5.poseNet(video , modelLoaded);
-poseNet.on('pose', gotPoses)
-}
-function gotPoses(results) {
-if(results.length > 0)
+function gotPoses(results)
 {
-console.log(results)
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX);
+    }
 }
-}
-function modelLoaded()
+
+
+function draw()
 {
-    console.log('PoseNet is initialized!');
+    background('#A4036F');
+    fill('#fff');
+    stroke('#048BA8')
+    text('Hi',noseX,noseY);
+    textSize(difference);
 }
